2edf9e0c4ef551b06c59410d1a5fe26e768ce724
Helder Faustini <helder.faustini.2023@yahoo.com>
2023-12-05 21:37:11 -0300

    teste

---
+++ ExString/src/ExString.java
@@ -0,0 +1,12 @@
+public class ExString {
+
+    public static void main(String[] args){
+        String nome = "Helder";
+        int comprimento = 0;
+
+        comprimento = nome.length();
+
+        System.out.println(" O nome " + nome + " tem " + comprimento + " letras");
+    }
+
+}
