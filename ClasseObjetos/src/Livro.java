bcc390c50579547e422cd5323f95ae601e3a06ca
Helder Faustini <helder.faustini.2023@yahoo.com>
2023-12-05 21:59:29 -0300

    teste

---
+++ ClasseObjetos/src/Livro.java
@@ -0,0 +1,23 @@
+public class Livro {
+    String titulo;
+    String autor;
+
+    void exibirInfo(){
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor: " + autor);
+    }
+
+
+
+
+    public static void main(String[] args) throws Exception {
+        
+        Livro meuLivro = new Livro();
+
+        meuLivro.titulo = "Java para Iniciantes";
+
+        meuLivro.autor = "Luan";
+
+        meuLivro.exibirInfo();
+    }
+}
