7054e93a03089cc88afe4534034d84a1ea4288af
HelderFaustini <helder.faustini.2023@gmail.com>
2023-12-07 21:13:09 -0300

    Quantas letras

---
+++ QuantasLetras/src/QuantasLetras.java
@@ -0,0 +1,11 @@
+public class QuantasLetras {
+    public static void main(String[] args) throws Exception {
+        
+        String nome = "José";
+        int tamanho = 0;
+
+        tamanho = nome.length();
+        
+        System.out.println("O nome " + nome + " tem " + tamanho + " letras");
+    }
+}
