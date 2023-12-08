6c8c0fd114b336991ccd417914bb6f0a6a7b5f90
HelderFaustini <helder.faustini.2023@gmail.com>
2023-12-07 21:43:57 -0300

    AreaRetangulo

---
+++ AreaRetangulo/src/AreaRetangulo.java
@@ -0,0 +1,11 @@
+public class AreaRetangulo {
+    public static void main(String[] args) throws Exception {
+
+        int comprimento = 10;
+        int altura = 5;
+        int area = comprimento * altura;
+
+
+        System.out.println("A área do retangulo é de: " + area + " cm²");
+    }
+}
